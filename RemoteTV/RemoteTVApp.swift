a0b15c81cf6c7739946bec13f53a39d3d247c84f
BogdanTarchenko <tarchenko.bogdan@outlook.com>
2025-05-26 17:14:01 +0700

    Initial Commit

---
+++ RemoteTV/RemoteTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteTVApp.swift
+//  RemoteTV
+//
+//  Created by Богдан Тарченко on 26.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct RemoteTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
